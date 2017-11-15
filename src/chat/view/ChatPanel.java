2176a5aeb7cc4bb1a7ceaeb24b2fa374a1370579
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2017-11-15 09:14:42 -0700

    initialized data mebers

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -3,8 +3,40 @@ package chat.view;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
-import
+import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
 
-public class ChatPanel {
+import chat.controller.ChatController;
 
+public class ChatPanel extends JPanel
+{
+	private ChatController appController;
+	private JButton chatutton;
+	private JTextField inputField;
+	private JTextArea hatArea;
+	private SpringLayout baseLayout;
+	
+	public ChatPanel(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		//initialize GUI data members
+		chatButton = new JButton("chat");
+		chatArea = new JTextArea(10, 25);
+		inputField = new JTextField(20);
+		appLayout = new SpringLayout();
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	
+	private void stupLayout()
+	
+	private void setupListeners()
+	
 }
