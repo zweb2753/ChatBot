c3ad4c64bc8b4a1c77ae8bf0fd01b511b83a9156
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2017-11-15 09:15:41 -0700

    uncommented the code

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -6,20 +6,20 @@ import javax.swing.JFrame;
 public class ChatFrame extends JFrame
 {
 	private ChatController appController;
-	//private ChatPanel appPanel;
+	private ChatPanel appPanel;
 	
 	public ChatFrame(ChatController appController)
 	{
 		super();
 		this.appController = appController;
-		//appPanel = new ChatPanel(appController);
+		appPanel = new ChatPanel(appController);
 		setupFrame();
 	}
 	
 	private void setupFrame() 
 	{
 		this.setResizable(false);
-		//this.setContentPane(appPanel);
+		this.setContentPane(appPanel);
 		this.setSize(500,500);
 		this.setTitle("Chatbot 2017");
 		this.setVisible(true);
