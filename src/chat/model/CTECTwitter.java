f6267f9aca9596a16157d028822ba4b7081cf1a8
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2018-02-21 09:30:42 -0700

    added tweet method

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -22,6 +22,18 @@ public class CTECTwitter
 	
 	public void sendTweet(String textToTweet)
 	{
-		
+		try
+		{
+			chatbotTwitter.updateStatus(textToTweet + " @ChatbotCTEC");
+		}
+		catch(TwitterException tweetError)
+		{
+			appController.handleErrors(tweetError);
+		}
+		catch(Exception otherError)
+		{
+			
+			appController.handleErrors(otherError);
+		}
 	}
 }
