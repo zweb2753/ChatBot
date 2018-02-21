9e481c1ac6e2c8a173f5e9d53076e323fcf2db30
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2018-02-21 08:28:00 -0700

    added buttons and button listener

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,7 +1,7 @@
 package chat.view;
 
 import javax.swing.JPanel;
-
+import javax.swing.*;
 import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JTextField;
@@ -15,6 +15,10 @@ import chat.controller.ChatController;
 
 public class ChatPanel extends JPanel 
 {
+	private JButton tweetButton;
+	private JButton searchButton;
+	private JButton saveButton;
+	private JButton loadButton;
 	private ChatController appController;
 	private JButton chatButton;
 	private JButton checkerButton;
@@ -29,23 +33,37 @@ public class ChatPanel extends JPanel
 		super();
 		this.appController = appController;
 		chatButton =  new JButton("chat");
+		loadButton = new JButton("load");
+		saveButton = new JButton("save");
+		tweetButton = new JButton("tweet");
+		searchButton = new JButton("search");
+		
 		inputField = new JTextField(20);
 		appLayout = new SpringLayout();
-		appLayout.putConstraint(SpringLayout.WEST, chatButton, 6, SpringLayout.EAST, inputField);
+		appLayout.putConstraint(SpringLayout.SOUTH, inputField, -103, SpringLayout.SOUTH, this);
 		appLayout.putConstraint(SpringLayout.SOUTH, chatButton, -10, SpringLayout.SOUTH, this);
-		appLayout.putConstraint(SpringLayout.SOUTH, inputField, -7, SpringLayout.SOUTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, chatButton, -10, SpringLayout.EAST, this);
 		chatArea = new JTextArea(10, 25);
 		chatScrollPane = new JScrollPane();
 		appLayout.putConstraint(SpringLayout.NORTH, inputField, 6, SpringLayout.SOUTH, chatScrollPane);
 		appLayout.putConstraint(SpringLayout.WEST, inputField, 0, SpringLayout.WEST, chatScrollPane);
+		appLayout.putConstraint(SpringLayout.EAST, inputField, 239, SpringLayout.WEST, chatScrollPane);
 		appLayout.putConstraint(SpringLayout.WEST, chatScrollPane, 25, SpringLayout.WEST, this);
 
 		setupPanel();
 		setupLayout();
 		setupListeners();
 		setupScrollPane();
-	}
+		
+		chatButton = new JButton("chat", new ImageIcon(getClass().getResource("/chat/view/images/chat.png")));
+		loadButton = new JButton("load", new ImageIcon(getClass().getResource("/chat/view/images/loadglasses.png")));
+		saveButton = new JButton("save", new ImageIcon(getClass().getResource("/chat/view/images/save icon.png")));
+		tweetButton = new JButton("tweet", new ImageIcon(getClass().getResource("/chat/view/images/tweet icon.png")));
+		searchButton = new JButton("search", new ImageIcon(getClass().getResource("/chat/view/images/search icon.png")));
+		chatArea = new JTextArea(10, 25);
 
+	}
+	
 	private void setupScrollPane()
 	{
 		chatScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
@@ -65,6 +83,10 @@ public class ChatPanel extends JPanel
 		this.add(inputField);
 		this.add(chatScrollPane);
 		this.add(chatButton);
+		this.add(loadButton);
+		this.add(saveButton);
+		this.add(tweetButton);
+		this.add(searchButton);
 		
 		chatArea.setEnabled(false);
 		chatArea.setEditable(false);
@@ -87,6 +109,38 @@ public class ChatPanel extends JPanel
 				String displayText = appController.interactWithChatbot(userText);
 				chatArea.append(displayText);
 				inputField.setText("");
+			}
+		});
+		
+		searchButton.addActionListener(new actionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				
+			}
+		});
+		
+		tweetButton.addActionListener(new actionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				
+			}
+		});
+		
+		saveButton.addActionListener(new actionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				
+			}
+		});
+				
+		loadButton.addActionListener(new actionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				
 			}
 		});
 	}
